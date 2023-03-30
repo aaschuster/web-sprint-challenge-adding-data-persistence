ead11de8079f6577eb2c1639fc7d02a2ad969309
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-30 09:47:52 -0400

    add tasks columns to migration file

--- data/migrations/20230330133450_build-tables.js
+++ data/migrations/20230330133450_build-tables.js
@@ -23,7 +23,19 @@ exports.up = async function(knex) {
     })
 
     .createTable("tasks", table => {
-        table.increments("task_id")
+        table.increments("task_id");
+        table.string("task_description")
+            .notNullable();
+        table.string("task_notes");
+        table.integer("task_completed")
+            .defaultTo(0);
+        table.integer("project_id")
+            .unsigned()
+            .notNullable()
+            .references("project_id")
+            .inTable("projects")
+            .onDelete("RESTRICT")
+            .onUpdate("RESTRICT");
     })
 
     .createTable("project_resources", table => {
