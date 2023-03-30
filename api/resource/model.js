dc3cfea1e64431edf07ac1ff1659a3ae8af875f6
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-30 10:16:46 -0400

    begin building resource model and seeds

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,11 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+function insert(resource) {
+    return db("resources").insert(resource);
+};
+
+function get() {
+    return db("resources");
+};
+
+module.exports = {insert, get};
--- api/server.js
+++ api/server.js
@@ -6,6 +6,9 @@ const server = express();
 
 server.use(express.json());
 
+const resourceRouter = require("./resource/router");
+server.use("/api/resources", resourceRouter);
+
 server.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         customMessage: "There was an issue with the server",
