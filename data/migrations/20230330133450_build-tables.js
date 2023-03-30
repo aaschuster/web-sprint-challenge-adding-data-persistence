f8c1a9ad3b53c3b2e27ba79cdbd12e44dfd958f7
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-30 09:41:20 -0400

    begin building migration file

---
+++ data/migrations/20230330133450_build-tables.js
@@ -0,0 +1,41 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema
+
+    .createTable("projects", table => {
+        table.increments("project_id");
+        table.string("project_name")
+            .notNullable();
+        table.string("description")
+        table.integer("project_completed")
+            .defaultTo(0);
+    })
+
+    .createTable("resources", table => {
+        table.increments("resource_id")
+    })
+
+    .createTable("tasks", table => {
+        table.increments("task_id")
+    })
+
+    .createTable("project_resources", table => {
+        table.increments("project_resource_id")
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema
+        dropTableIfExists("project_resources");
+        dropTableIfExists("tasks");
+        dropTableIfExists("resources");
+        dropTableIfExists("projects");
+
+};
